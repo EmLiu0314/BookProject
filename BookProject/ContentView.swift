48aaec5dbfb06e492edff99927b109b0d78b94f7
EmLiu0314 <emilyliu52106@gmail.com>
2024-03-19 23:02:35 -0500

    Simple Interface

--- BookProject/ContentView.swift
+++ BookProject/ContentView.swift
@@ -2,23 +2,80 @@
 //  ContentView.swift
 //  BookProject
 //
-//  Created by Liu, Emily on 3/19/24.
-//
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Book: Identifiable {
+    let id = UUID()
+    let title: String
+    let author: String
+    let imageName: String
 }
 
-#Preview {
-    ContentView()
-}
+struct ContentView: View {
+        let books = [
+            Book(title: "Babel", author: "RF Kuang", imageName: "Babel"),
+            Book(title: "The Secret History", author: "Donna Tartt", imageName: "SecretHistory"),
+            Book(title: "Song of Åchilles", author: "Madelline Miller", imageName: "achilles"),
+            Book(title: "Pride and Prejudice", author: "Jane Austen", imageName: "pride_prejudice")
+        ]
+        
+        var body: some View {
+            NavigationView {
+                List(books) { book in
+                    NavigationLink(destination: BookDetailView(book: book)) {
+                        BookRow(book: book)
+                    }
+                }
+                .navigationTitle("Books")
+            }
+        }
+    }
+
+    struct BookRow: View {
+        let book: Book
+        
+        var body: some View {
+            HStack {
+                Image(book.imageName)
+                    .resizable()
+                    .frame(width: 50, height: 70)
+                VStack(alignment: .leading) {
+                    Text(book.title)
+                        .font(.headline)
+                    Text(book.author)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+    }
+
+    struct BookDetailView: View {
+        let book: Book
+        
+        var body: some View {
+            VStack {
+                Image(book.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                Text(book.title)
+                    .font(.title)
+                    .padding(.top)
+                Text("by \(book.author)")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Spacer()
+            }
+            .navigationTitle(book.title)
+        }
+    }
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
+                     
