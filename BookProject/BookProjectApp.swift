c7b8449b9b20d4c97e5e52656c326e2cd0013d5b
EmLiu0314 <emilyliu52106@gmail.com>
2024-03-19 22:55:30 -0500

    Initial Commit

---
+++ BookProject/BookProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookProjectApp.swift
+//  BookProject
+//
+//  Created by Liu, Emily on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BookProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
